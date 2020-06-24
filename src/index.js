1e6306513761fe8f7b410c68ec8ac8df4fe31687
AchuAnnaAntony <achuannaantony@gmail.com>
2020-06-24 11:56:27 -0700

    Multi level Dropdown

--- src/index.js
+++ src/index.js
@@ -1,17 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import DropdownMenu from './DropdownMenu';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+var destination = document.querySelector('#container');
+
+
+ReactDOM.render( <
+  DropdownMenu config = {
+    [{
+        "title": "Home",
+        "submenu": null
+     },
+     {
+        "title": "Category",
+        "submenu": [{
+            "title": "Gents",
+            "submenu": [{
+                "title": "Shirts",
+                "submenu": null
+              },
+              {
+                "title": "Pants",
+                "submenu": null
+              }]
+          },
+          {
+            "title": "Ladies",
+            "submenu": [{
+                "title": "Dresses",
+                "submenu": null
+              },
+              {
+                "title": "Bags",
+                "submenu": null
+              }]
+          },
+          {
+            "title": "Kids",
+            "submenu": [{
+                "title": "Pajamas",
+                "submenu": null
+              },
+              {
+                "title": "Shirts",
+                "submenu": null
+              }]
+          }]
+     }
+  ]}
+  />, destination);
+  
